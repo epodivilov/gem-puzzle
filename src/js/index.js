0bc9dc6e13578e4cc341714121436b2b6d4246f4
Подивилов Евгений <evgeniy.podivilov@gmail.com>
2016-08-01 15:19:31 +0500

    Added base class and initial logic

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,20 @@
 import '../styles/index.styl'
+import Game from './Game'
+
+window.onload = function () {
+
+    const   playfield = document.getElementById('playfield'),
+            stepsBar =  document.getElementById('currentsteps'),
+            resetBtn =  document.getElementById('reset');
+
+    const game = new Game({
+        field: playfield,
+        steps: stepsBar
+    })
+    
+    game.initialize();
+
+    resetBtn.addEventListener('click', () => {
+        game.initialize();
+    })
+}
