3caa750fea610cb5043702ec065b19f370f28012
Подивилов Евгений <evgeniy.podivilov@gmail.com>
2016-08-02 14:25:41 +0500

    Added method checking game over and counting steps

--- src/js/Game/Game.js
+++ src/js/Game/Game.js
@@ -4,7 +4,9 @@ export default class Game {
     constructor(props) {
         this.field = props.field;
         this.steps = props.steps;
+        this.gameOver = props.gameOver;
         this.tiles = [];
+        this.count = 0;
     }
     
     initialize() {
@@ -21,7 +23,15 @@ export default class Game {
             e.stopPropagation();
             this.field.removeChild(this.tiles[3][3].el)
             this.tiles[3][3] = undefined;
-            this.field.addEventListener('click', this.onClick.bind(this))
+            this.field.onclick = () => {
+                for (let row = 0; row < 4; row++) {
+                    for (let col = 0; col < 4; col++) {
+                        if (this.tiles[row][col] && event.target === this.tiles[row][col].el) {
+                            return this.moveTile(row, col);
+                        }
+                    }
+                }
+            };
         })
 
         for (let row = 0; row < 4; row++) {
@@ -32,7 +42,6 @@ export default class Game {
         }
 
         this.render()
-
     }
 
     clear() {
@@ -42,8 +51,9 @@ export default class Game {
             })
         })
         this.tiles = [];
-        this.steps.innerHTML = 0;
-        this.field.removeEventListener('click', this.onClick.bind(this))
+        this.count = 0;
+        this.gameOver.style.zIndex = '-1';
+        this.field.onclick = undefined;
     }
 
     render() {
@@ -52,16 +62,7 @@ export default class Game {
                 if (column) column.render(dX, dY)
             })
         })
-    }
-
-    onClick(event) {
-        for (let row = 0; row < 4; row++) {
-            for (let col = 0; col < 4; col++) {
-                if (this.tiles[row][col] && event.target === this.tiles[row][col].el) {
-                    return this.moveTile(row, col);
-                }
-            }
-        }
+        this.steps.innerHTML = this.count;
     }
 
     moveTile(dX, dY) {
@@ -83,7 +84,23 @@ export default class Game {
             this.tiles[dX][dY - 1] = currentTile;
             this.tiles[dX][dY] = undefined;
         }
+        
+        if (this.isGameOver()) {
+            this.gameOver.style.zIndex = '100';
+        }
+
+        this.count++;
 
         this.render()
+    }
+
+    isGameOver() {
+        for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
+                if ((row * 4 + col + 1) != 16 && !this.tiles[row][col]) return false;
+                if ((row * 4 + col + 1) != 16 && this.tiles[row][col].label !== (row * 4 + col + 1)) return false;
+            }
+        }
+        return true;
     }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -4,12 +4,14 @@ import Game from './Game/Game'
 window.onload = function () {
 
     const   playfield = document.getElementById('playfield'),
+            gameOver =  document.getElementById('gameover'),
             stepsBar =  document.getElementById('currentsteps'),
             resetBtn =  document.getElementById('reset');
 
     const game = new Game({
         field: playfield,
-        steps: stepsBar
+        steps: stepsBar,
+        gameOver: gameOver
     })
     
     game.initialize();
